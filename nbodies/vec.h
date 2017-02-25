7ea483862dcd77124e366fb54359f20cb3a30843
Brian Liao <phyred23@gmail.com>
2017-02-25 12:29:33 -0800

    add operators to vec

--- nbodies/vec.h
+++ nbodies/vec.h
@@ -8,12 +8,6 @@ struct tvec2 {
 	template<typename U>
 	tvec2(tvec2<U> v) : x(T(v.x)), y(T(v.y)) {}
 
-	// polar ? need help
-	T r, theta;
-	tvec2(T v = T(0) : r(v), theta(v) {}
-	tvec2(T r, T theta) : r(r), theta(theta) {}
-	template<typename U>
-	tvec2(tvec2<U> v) : : r(T(v.r), theta(T(v.theta)) {}
 };
 typedef tvec2<float> vec2;
 
@@ -36,4 +30,29 @@ inline tvec2<T> operator* (const T& b, const tvec2<T>& a) {
 	return tvec2<T>(b*a.x, b*a.y);
 }
 
-// what to do for polar here?
+template<typename T>
+inline float length (const tvec2<T>& v) {
+	return sqrt(pow(v.x, 2) + pow(v.y, 2));
+}
+
+// change
+template<typename T>
+inline tvec2<T> to_polar(const tvec2<T>& v) {
+	return length(v);
+}
+
+// change
+template<typename T>
+inline float from_polar(const tvec2<T>& v) {
+	return tvec2<T>(x * cos(y), x * sin(y));
+}
+
+/*
+public Polar toPolar() {
+	return new Polar(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)), Math.toDegrees(Math.atan(y / x)));
+}
+
+public Cartesian toCartesian() {
+	return new Cartesian(r * Math.cos(Math.toRadians(theta)), r * Math.sin(Math.toRadians(theta)));
+}
+}*/
